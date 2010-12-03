a151eee854610a54c290dbbaa666444229798039
Mat Schaffer <mat@schaffer.me>
2010-12-03 07:36:26 -0500

    Roughly finished zelda. Copped out on the end part until I can listen to the original.

--- examples/zelda.rb
+++ examples/zelda.rb
@@ -1,6 +1,6 @@
 require 'collavoce'
 
-class Melody < Collavoce::Voice
+class Melody1 < Collavoce::Voice
   channel 1
   notes %w(G#e Be C#5e D5e) 
 
@@ -21,6 +21,7 @@ class Melody < Collavoce::Voice
     2.times do
       play_with { notes[0].dim! }
     end
+    play(4)
   end
 end
 
@@ -38,10 +39,20 @@ end
 
 class BassPart2 < Collavoce::Voice
   channel 2
-  notes %w(Eb3w G3h Ebqe D)
+  notes %w(Eb3w G3qe Ebqe D)
+  def run
+    play
+    mod_notes(0, 2, 3, &:dim!)
+    play
+  end
+end
+
+class BassPart3 < Collavoce::Voice
+  channel 2
+  notes %w(Ch A3h Gb3w)
 end
 
 Collavoce::Tracker.new(
-  [Melody],
-  [BassPart1, BassPart2]
+  [Melody1],
+  [BassPart1, BassPart2, BassPart3]
 ).run(240)
